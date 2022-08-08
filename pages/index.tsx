9c600611fc514eae08ef8e6b4fbd8698aee29576
Nathalia <mdc.nathalia@gmail.com>
2022-08-08 16:39:39 +0300

    Remove unused code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,8 @@
 import React, { useState, useEffect } from "react";
 import type { NextPage } from "next";
-import {
-  AppBar,
-  Toolbar,
-  Stack,
-  Button,
-  Typography,
-  CircularProgress,
-} from "@mui/material";
-import axios from "axios";
-import axiosRetry from "axios-retry";
+import { AppBar, Toolbar, Stack, Button } from "@mui/material";
 
 const DEFAULT_SEARCH = "?folder=/home/ubuntu/workspace/example-1";
-
-axiosRetry(axios, {
-  retries: 20,
-  retryDelay: (retryCount) => {
-    return retryCount * 500;
-  },
-  retryCondition: () => true,
-});
 
 interface IApp {
   name: string;
@@ -37,7 +20,6 @@ const apps: IApp[] = [
 ];
 
 const Home: NextPage = () => {
-  const [editorAvailable, setEditorAvailable] = useState(true);
   const [protocol, setProtocol] = useState<string | undefined>();
   const [hostname, setHostname] = useState<string | undefined>();
 
@@ -46,74 +28,45 @@ const Home: NextPage = () => {
     setHostname(window.location.hostname);
   }, []);
 
-  // useEffect(() => {
-  //   if (protocol && hostname) {
-  //     axios.get(`${protocol}//${hostname}:${editor.port}`).then((_) => {
-  //       setEditorAvailable(true);
-  //     });
-  //   }
-  // }, [protocol, hostname]);
-
   return (
     <>
-      {!editorAvailable && <Loading />}
-      {editorAvailable && (
-        <>
-          <AppBar elevation={0} sx={{ backgroundColor: "#1b1e2e" }}>
-            <Toolbar variant="dense">
-              <Stack
-                direction="row"
-                justifyContent="flex-end"
-                flexGrow={1}
-                gap={2}
+      <AppBar elevation={0} sx={{ backgroundColor: "#1b1e2e" }}>
+        <Toolbar variant="dense">
+          <Stack direction="row" justifyContent="flex-end" flexGrow={1} gap={2}>
+            {apps.map((app, index) => (
+              <Button
+                key={index}
+                variant="outlined"
+                size="small"
+                component="a"
+                href={`${protocol}//${hostname}:${app.port}`}
+                target="_blank"
+                sx={{
+                  borderColor: "#e83a74",
+                  color: "#e83a74",
+                  ":hover": {
+                    borderColor: "#e83a74",
+                  },
+                  fontWeight: 700,
+                }}
               >
-                {apps.map((app, index) => (
-                  <Button
-                    key={index}
-                    variant="outlined"
-                    size="small"
-                    component="a"
-                    href={`${protocol}//${hostname}:${app.port}`}
-                    target="_blank"
-                    sx={{
-                      borderColor: "#e83a74",
-                      color: "#e83a74",
-                      ":hover": {
-                        borderColor: "#e83a74",
-                      },
-                      fontWeight: 700,
-                    }}
-                  >
-                    {app.name}
-                  </Button>
-                ))}
-              </Stack>
-            </Toolbar>
-          </AppBar>
-          <iframe
-            src={`${protocol}//${hostname}:${editor.port}/${DEFAULT_SEARCH}`}
-            width="100%"
-            style={{
-              position: "fixed",
-              height: "calc(100vh - 48px)",
-              top: 48,
-              border: "none",
-            }}
-          />
-        </>
-      )}
+                {app.name}
+              </Button>
+            ))}
+          </Stack>
+        </Toolbar>
+      </AppBar>
+      <iframe
+        src={`${protocol}//${hostname}:${editor.port}/${DEFAULT_SEARCH}`}
+        width="100%"
+        style={{
+          position: "fixed",
+          height: "calc(100vh - 48px)",
+          top: 48,
+          border: "none",
+        }}
+      />
     </>
-  );
-};
-
-const Loading = () => {
-  return (
-    <Stack alignItems="center" justifyContent="center" gap={4} height="100vh">
-      <Typography variant="h2" justifyContent="center">
-        Creating stack
-      </Typography>
-      <CircularProgress size={60} thickness={5} />
-    </Stack>
   );
 };
 
